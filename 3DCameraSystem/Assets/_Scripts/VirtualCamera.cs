2ea270c6c7d90a03a6542c159452c6f1bb622d0d
martinka-h <beaublueduck@proton.me>
2024-05-20 00:14:56 +0300

    add and set transposer and composer if not present

--- 3DCameraSystem/Assets/_Scripts/VirtualCamera.cs
+++ 3DCameraSystem/Assets/_Scripts/VirtualCamera.cs
@@ -2,34 +2,34 @@ using UnityEngine;
 using Cinemachine;
 using static Cinemachine.CinemachineTransposer;
 
+// Make sure that the Cinemachine camera prefab has "Save During Play" enabled.
+// The changes transposer will be made only if transposer is already present.
+
 namespace CameraSystem {
     public class VirtualCamera : MonoBehaviour {
-
-        private Vector3 followOffset = new Vector3(0, 40, -26);
-
         private void Start()
         {
             CinemachineVirtualCamera camera = GetComponent<CinemachineVirtualCamera>();
 
-            Transform cameraSystem = new GameObject("CameraSystem").transform;
+            // Set camera follow and lookat if empty
+            if (camera.Follow == null) {
+                Transform cameraSystem = new GameObject("CameraSystem").transform;
+                camera.Follow = cameraSystem;
+                camera.LookAt = cameraSystem;
+            }    
 
-            camera.Follow = cameraSystem;
-            camera.LookAt = cameraSystem;
-
-            // Add and set up Cinemachine Transposer
+            // Add and set up Cinemachine Transposer if not present
             CinemachineTransposer transposer = camera.GetCinemachineComponent<CinemachineTransposer>();
 
             if (transposer == null) {
                 camera.AddCinemachineComponent<CinemachineTransposer>();
+                transposer = camera.AddCinemachineComponent<CinemachineTransposer>();
+                transposer.m_BindingMode = BindingMode.LockToTargetWithWorldUp;
+                transposer.m_FollowOffset = new Vector3(0, 40, -26);
             }
 
-            transposer = camera.AddCinemachineComponent<CinemachineTransposer>();
-            transposer.m_BindingMode = BindingMode.LockToTargetWithWorldUp;
-            transposer.m_FollowOffset = followOffset;
-
-            // Add Cinemachine Composer
+            // Add Cinemachine Composer if not present
             CinemachineComposer composer = camera.GetCinemachineComponent<CinemachineComposer>();
-
             if (composer == null) {
                 camera.AddCinemachineComponent<CinemachineComposer>();
             }
