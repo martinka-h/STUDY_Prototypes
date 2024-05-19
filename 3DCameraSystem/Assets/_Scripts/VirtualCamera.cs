188f0ee7cd80f561b60aa452f8844ed7a186ebef
martinka-h <beaublueduck@proton.me>
2024-05-19 23:53:54 +0300

    Cinemachine camera prefab

--- 3DCameraSystem/Assets/_Scripts/VirtualCamera.cs
+++ 3DCameraSystem/Assets/_Scripts/VirtualCamera.cs
@@ -4,22 +4,20 @@ using static Cinemachine.CinemachineTransposer;
 
 namespace CameraSystem {
     public class VirtualCamera : MonoBehaviour {
+
         private Vector3 followOffset = new Vector3(0, 40, -26);
-        private CinemachineTransposer transposer;
 
         private void Start()
         {
-            Debug.Log(followOffset);
-            CinemachineVirtualCamera camera = new GameObject("CinemachineVirtualCamera").AddComponent<Cinemachine.CinemachineVirtualCamera>();
+            CinemachineVirtualCamera camera = GetComponent<CinemachineVirtualCamera>();
+
             Transform cameraSystem = new GameObject("CameraSystem").transform;
 
             camera.Follow = cameraSystem;
             camera.LookAt = cameraSystem;
 
-            camera.transform.position = new Vector3(0, 0, -10);
-
             // Add and set up Cinemachine Transposer
-            transposer = camera.GetCinemachineComponent<CinemachineTransposer>();
+            CinemachineTransposer transposer = camera.GetCinemachineComponent<CinemachineTransposer>();
 
             if (transposer == null) {
                 camera.AddCinemachineComponent<CinemachineTransposer>();
