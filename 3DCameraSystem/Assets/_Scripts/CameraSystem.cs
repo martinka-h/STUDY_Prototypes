d0fceeb4acddd05f5688dedefbdb0d83d405c8b3
martinka-h <beaublueduck@proton.me>
2024-05-20 01:15:34 +0300

    edge scrolling

--- 3DCameraSystem/Assets/_Scripts/CameraSystem.cs
+++ 3DCameraSystem/Assets/_Scripts/CameraSystem.cs
@@ -5,11 +5,23 @@ namespace CameraSystem {
 
         [SerializeField] private float moveSpeed = 50f;
         [SerializeField] private float rotateSpeed = 50f;
+        [SerializeField] int edgeScrollSize = 10;
+
+        [SerializeField] private bool useEdgeScrolling = true;
 
         private void Update()
         {
             Vector3 inputDir = new Vector3(0, 0, 0);
 
+            // edge scrolling
+            if (useEdgeScrolling) {
+                if (Input.mousePosition.x < edgeScrollSize) inputDir.x = -1f;
+                if (Input.mousePosition.y < edgeScrollSize) inputDir.z = -1f;
+                if (Input.mousePosition.x > Screen.width - edgeScrollSize) inputDir.x = +1f;
+                if (Input.mousePosition.y > Screen.height - edgeScrollSize) inputDir.z += +1f;
+            }
+
+            // move
             if (Input.GetKey(KeyCode.W)) inputDir.z = +1f;
             if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
             if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
@@ -18,11 +30,14 @@ namespace CameraSystem {
             Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
             transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+            // rotate
             float rotateDir = 0f;
             if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
             if (Input.GetKey(KeyCode.E)) rotateDir = -1f;
 
-            transform.eulerAngles += new Vector3(0,rotateDir * rotateSpeed * Time.deltaTime,0);
+            transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
+
+
         }
     }
-} 
+}
